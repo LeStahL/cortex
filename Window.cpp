d985b39bfb13868bef2104d5e2d04f382ee0c469
Alexander Kraus <nr4@z10.info>
2020-03-24 16:45:55 +0100

    implemented parts of the selector into window class

--- Window.cpp
+++ Window.cpp
@@ -1,5 +1,9 @@
 #include "Window.hpp"
 
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+
 LRESULT CALLBACK callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch(uMsg)
@@ -13,7 +17,53 @@ LRESULT CALLBACK callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 Window::Window(const char* _title)
     : title(_title)
+    , isSelector(true)
 {
+    // Determine supported display device modes
+    DEVMODE dm = { 0 };
+    dm.dmSize = sizeof(dm);
+    
+    // Get number of supported device modes
+    for(nResolutions = 0; EnumDisplaySettings(NULL, nResolutions, &dm) != 0; ++nResolutions);
+    
+    // Allocate arrays
+    int *widths = (int*) malloc(nResolutions * sizeof(int)),
+        *heights = (int*) malloc(nResolutions * sizeof(int)),
+        *rates = (int*)malloc(nResolutions * sizeof(int)),
+        defaultIndex = 0;
+    
+    // Fill arrays
+    for(int i = 0; i < nResolutions; ++i) 
+    {
+        EnumDisplaySettings(NULL, i, &dm);
+        
+        // Check if settings are already in list
+        bool isInList = false;
+        for(int k = 0; k < i; ++k)
+        {
+            if(widths[k] == dm.dmPelsWidth && heights[k] == dm.dmPelsHeight && rates[k] == dm.dmDisplayFrequency)
+            {
+                isInList = true;
+                break;
+            }
+        }
+        
+        // Add to list if not present
+        if(!isInList)
+        {
+            widths[nUniqueResolutions] = dm.dmPelsWidth;
+            heights[nUniqueResolutions] = dm.dmPelsHeight;
+            rates[nUniqueResolutions] = dm.dmDisplayFrequency;
+            
+            // Mark default (720p)
+            if(dm.dmPelsWidth == 1280 && dm.dmPelsHeight == 720 && dm.dmDisplayFrequency == 60)     
+                defaultIndex = nUniqueResolutions;
+            
+            ++nUniqueResolutions;
+        }
+    }
+    
+    // Build selector window
     WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
@@ -25,15 +75,28 @@ Window::Window(const char* _title)
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = NULL;
 	wc.lpszMenuName = NULL;
-	wc.lpszClassName = this->title;
+	wc.lpszClassName = title;
 	wc.hIconSm = NULL;
 
 	RegisterClassEx(&wc);
     
-    this->handle = CreateWindowEx(0, this->title, this->title, WS_OVERLAPPEDWINDOW, 200, 200, 341, 150, NULL, NULL, this->instance, 0);
+    handle = CreateWindowEx(0, title, title, WS_OVERLAPPEDWINDOW, 200, 200, 300, 360, NULL, NULL, instance, 0);
     
-    ShowWindow(this->handle, TRUE);
-    UpdateWindow(this->handle);
+    resolutionLabelHandle = CreateWindow(WC_STATIC, "Resolution: ", WS_VISIBLE | WS_CHILD | SS_LEFT, 10, 15, 100, 100, handle, NULL, instance, NULL);
+    
+    resolutionDropdownHandle = CreateWindow(WC_COMBOBOX, "", CBS_DROPDOWN | CBS_HASSTRINGS | WS_CHILD | WS_OVERLAPPED | WS_VISIBLE, 100, 10, 175, nUniqueResolutions*15, handle, (HMENU)RESOLUTION_COMBOBOX, instance, NULL);
+    
+    // Add items to resolution combo box and select full HD
+    for(int i=0; i<nUniqueResolutions; ++i)
+    {
+        char name[1024];
+        sprintf(name, "%d x %d @ %d Hz", widths[i], heights[i], rates[i]);
+        SendMessage(resolutionDropdownHandle, (UINT) CB_ADDSTRING, (WPARAM) 0, (LPARAM) name);
+    }
+	SendMessage(resolutionDropdownHandle, CB_SETCURSEL, defaultIndex, 0);
+    
+    ShowWindow(handle, TRUE);
+    UpdateWindow(handle);
     
     windowInstance = this;
 }
--- Window.hpp
+++ Window.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Windows.h"
+#	include <commctrl.h>
+#define RESOLUTION_COMBOBOX 0x1
 
 class Window
 {
@@ -8,11 +10,33 @@ public:
     Window(const char *title = " :: Team210 - Go, make a demo :: ");
     ~Window();
     
+    // Window related
     const char *title;
     HWND handle, 
         *controlHandles;
     HDC deviceContext;
     HINSTANCE instance;
+    
+    // OpenGL related
+    
+    // Selector related
+    bool isSelector,
+        recording;
+    const char *outputDirectory;
+    int nResolutions = 0,
+        nUniqueResolutions = 0;
+    HWND resolutionLabelHandle,
+        resolutionDropdownHandle,
+        muteCheckboxHandle,
+        sfxBufferSizeLabelHandle,
+        sfxBufferSizedropdownHandle,
+        recordCheckboxHandle,
+        recordOutputDirectoryLabelHandle,
+        recordOutputDirectoryTextboxHandle,
+        recordOutputFramerateLabelHandle,
+        recordOutputFramerateDropdownHandle,
+        startButtonHandle,
+        team210CreditLabel;
 };
 
 static Window *windowInstance = nullptr;
